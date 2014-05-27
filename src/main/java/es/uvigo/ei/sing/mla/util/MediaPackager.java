01c2467c43e193640775950dd2c48b23677ccc34
mirkeet <afnandin@esei.uvigo.es>
2014-05-27 21:31:55 +0200

    Added support for tar uploading/untar

--- src/main/java/es/uvigo/ei/sing/mla/util/MediaPackager.java
+++ src/main/java/es/uvigo/ei/sing/mla/util/MediaPackager.java
@@ -3,12 +3,15 @@ package es.uvigo.ei.sing.mla.util;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
@@ -24,15 +27,16 @@ public class MediaPackager {
 					.substring(
 							media.getName().toLowerCase().lastIndexOf(".") + 1);
 
+			File file = new File(media.getName());
+
 			switch (extension) {
 			case "zip":
-				File file = new File(media.getName());
-
 				try {
 					FileUtils.writeByteArrayToFile(file, media.getByteData());
 					ZipFile zipfile = new ZipFile(file);
 
-					for (Enumeration<? extends ZipEntry>  e = zipfile.entries(); e.hasMoreElements();) {
+					for (Enumeration<? extends ZipEntry> e = zipfile.entries(); e
+							.hasMoreElements();) {
 						ZipEntry entry = (ZipEntry) e.nextElement();
 						unzipEntry(zipfile, entry, outputDirectory);
 					}
@@ -43,6 +47,21 @@ public class MediaPackager {
 				break;
 
 			case "tar":
+				try {
+					FileUtils.writeByteArrayToFile(file, media.getByteData());
+					TarArchiveInputStream tais = new TarArchiveInputStream(
+							new FileInputStream(file));
+
+					TarArchiveEntry entry;
+
+					while ((entry = tais.getNextTarEntry()) != null) {
+						untarEntry(entry, outputDirectory);
+					}
+
+				} catch (IOException e1) {
+				}
+
+				break;
 
 			default:
 				throw new InvalidFormatException("");
@@ -50,14 +69,37 @@ public class MediaPackager {
 		}
 	}
 
-	private static void unzipEntry(ZipFile zipfile, ZipEntry entry,
-			File outputDir) throws IOException {
+	private static void untarEntry(TarArchiveEntry entry, File outputDirectory) {
 		if (entry.isDirectory()) {
-			createDirectory(new File(outputDir, entry.getName()));
+			createDirectory(new File(outputDirectory, entry.getName()));
 			return;
 		}
 
-		File outputFile = new File(outputDir, entry.getName());
+		File outputFile = new File(outputDirectory, entry.getName());
+
+		if (!outputFile.getParentFile().exists()) {
+			createDirectory(outputFile.getParentFile());
+		}
+
+		try (BufferedInputStream inputStream = new BufferedInputStream(
+				new FileInputStream(entry.getFile()));
+				BufferedOutputStream outputStream = new BufferedOutputStream(
+						new FileOutputStream(outputFile))) {
+
+			IOUtils.copy(inputStream, outputStream);
+		} catch (Exception e) {
+
+		}
+	}
+
+	private static void unzipEntry(ZipFile zipfile, ZipEntry entry,
+			File outputDirectory) throws IOException {
+		if (entry.isDirectory()) {
+			createDirectory(new File(outputDirectory, entry.getName()));
+			return;
+		}
+
+		File outputFile = new File(outputDirectory, entry.getName());
 
 		if (!outputFile.getParentFile().exists()) {
 			createDirectory(outputFile.getParentFile());
